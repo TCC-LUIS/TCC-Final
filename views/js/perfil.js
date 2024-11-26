06668b903071bbcbfb41f0964d7f9847045aa16b
Gabriel Henrique <106666571+Purolinho@users.noreply.github.com>
2024-11-26 12:07:23 -0300

    Add files via upload

---
+++ views/js/perfil.js
@@ -0,0 +1,15 @@
+$("#carrinho").on("click", () => {
+    location.href = "/gabrielJobs/Implementando/views/html/carrinho.html"
+})
+
+$("#publicar").on("click", () => {
+    location.href = "/gabrielJobs/Implementando/views/html/publicar_livro.html"
+})
+
+$("#img-conta").on("click", () => {
+    location.href = "/gabrielJobs/Implementando/views/html/index.html"
+})
+
+$("#sair").on("click", () => {
+    location.href = "/gabrielJobs/Implementando/views/html/login.html"
+})
